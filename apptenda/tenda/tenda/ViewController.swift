bd6457abc6069574917ce51e62f8b4e8b6212713
antolugo <corapi@live.it>
2016-03-31 21:51:04 +0200

    nouva cartella apptenda

---
+++ apptenda/tenda/tenda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tenda
+//
+//  Created by Anto on 31/03/16.
+//  Copyright © 2016 Anto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
