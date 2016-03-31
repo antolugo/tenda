e9cf14ca973bfb54e6d95030e248bb03e9f14b67
Domenico Corapi <dcorapi@yammer-inc.com>
2016-03-31 22:44:43 +0100

    Increased delay

--- tesi_automatica/tesi_automatica.ino
+++ tesi_automatica/tesi_automatica.ino
@@ -1,31 +1,3 @@
-/**************************************************************
- * Blynk is a platform with iOS and Android apps to control
- * Arduino, Raspberry Pi and the likes over the Internet.
- * You can easily build graphic interfaces for all your
- * projects by simply dragging and dropping widgets.
- *
- *   Downloads, docs, tutorials: http://www.blynk.cc
- *   Blynk community:            http://community.blynk.cc
- *   Social networks:            http://www.fb.com/blynkapp
- *                               http://twitter.com/blynk_app
- *
- * Blynk library is licensed under MIT license
- * This example code is in public domain.
- *
- **************************************************************
- *
- * This example shows how to use ESP8266 Shield via Hardware Serial
- * (on Mega, Leonardo, Micro...) to connect your project to Blynk.
- *
- * Note: Ensure a stable serial connection to ESP8266!
- *       Firmware version 1.0.0 (AT v0.22) or later is needed.
- *       You can change ESP baud rate. Connect to AT console and call:
- *           AT+UART_DEF=115200,8,1,0,0
- *
- * Change WiFi ssid, pass, and Blynk auth token to run :)
- * Feel free to apply it to any other example. It's simple!
- *
- **************************************************************/
 //#define BLYNK_DEBUG
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <ESP8266_HardSer.h>
@@ -146,5 +118,5 @@ void logic() {
   BLYNK_LOG("Fine corsa Up: %d",fineCorsaU);
   BLYNK_LOG("Fine corsa Down: %d",fineCorsaD);
   
-  delay(10);
+  delay(1000);
 }
